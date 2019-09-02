7054d76744e563ef9facb86692a49807a46c7023
SF <sf@viewar.com>
2019-09-02 16:21:52 +0200

    move configs main file to '/src'

---
+++ src/webpack.config.js
@@ -0,0 +1,21 @@
+const merge = require('webpack-merge')
+
+const common = require('./common.config')
+const production = require('./production.config')
+const develop = require('./develop.config')
+
+module.exports = (env) => {
+  if (env === 'production') {
+    console.log('[ViewAR] Using production mode.')
+    return merge(common.config, production.config)
+  }
+
+  const developConfig = merge(common.config, develop.config)
+  if (env === 'development_mock') {
+    console.log('[ViewAR] Using development mock mode.')
+    return merge(developConfig, develop.mock)
+  }
+
+  console.log('[ViewAR] Using development core mode.')
+  return merge(developConfig, develop.core)
+}
