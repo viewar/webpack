53903f1d15ab7cf33700be1f0845d82b3fedd334
SF <sf@viewar.com>
2019-09-03 15:41:22 +0200

    return proised config after `await errorOnUsedPort()`

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -3,8 +3,9 @@ const merge = require('webpack-merge')
 const common = require('./env/common.config')
 const production = require('./env/production.config')
 const develop = require('./env/develop.config')
+const { errorOnUsedPort } = require('./utils')
 
-module.exports = (env) => {
+const getMergedConfig = (env, ...args) => {
   if (env === 'production') {
     console.log('[ViewAR] Using production mode.')
     return merge(common.config, production.config)
@@ -19,3 +20,17 @@ module.exports = (env) => {
   console.log('[ViewAR] Using development core mode.')
   return merge(developConfig, develop.core)
 }
+
+/**
+ * @function
+ * @name @viewar/webpack/webpack.config.js
+ * @returns {Promise} webpack config
+ */
+module.exports = async (env) => {
+  if (env !== 'production') {
+    await errorOnUsedPort()
+  }
+  return getMergedConfig(env)
+}
+
+module.exports.getMergedConfig = getMergedConfig
