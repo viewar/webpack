9784af7110270acd2fb898d6ae364a7d518ec079
SF <sf@viewar.com>
2019-10-17 11:57:58 +0200

    lint:  remove rule for "node/no-missing-import"

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,15 @@
+/** set ESLINT env vars
+ *    used by #eslint-import-resolver-webpack'
+ */
+process.env.NODE_ENV = 'test';
+process.env.WEBPACK_PATH = 'test';
+
 module.exports = {
-  extends: ['viewar/env/react'],
+  extends: ['./node_modules/@viewar/config-eslint/env/react'],
   rules: {
     semi: ['error', 'never'],
-    "react/jsx-filename-extension": 0
+    "react/jsx-filename-extension": 0,
+    // conflicts between webpack- and node- resolver
+    "node/no-missing-import": 0,
   }
 }
