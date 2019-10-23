4624c10806cec92e9cad07791de23ab7fb3e2069
viewar_sf <dev@project23.org>
2019-10-23 20:35:59 +0200

    fix paths after move

--- test/App/App.jsx
+++ test/App/App.jsx
@@ -1,9 +1,10 @@
 import React, { PureComponent } from 'react'
 
-import styles from 'App.scss' // eslint-disable-line no-unused-vars
 import Test from 'components/Test.jsx'
 
-require('../remoteConsole').remoteConsoleInjector()
+import styles from './App.scss' // eslint-disable-line no-unused-vars
+
+require('../../remoteConsole').remoteConsoleInjector()
 
 class App extends PureComponent {
   render() {
@@ -12,7 +13,7 @@ class App extends PureComponent {
 
     return (
       <>
-        <h1>@viewar/webpack up and running!</h1>
+        <h1 id="app_headline">@viewar/webpack up and running!</h1>
         <Test />
       </>
     )
