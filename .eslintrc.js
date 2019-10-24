ceb2754e38f315f8ab8e2286c4d84f9c23d2eb95
SF <sf@viewar.com>
2019-10-24 15:16:10 +0200

    fix merge errors and stableize/clean up current version

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
 module.exports = {
-  extends: ['@viewar/config-eslint/env/react'],
+  extends: [
+    require.resolve('@viewar/config-eslint/env/react')
+  ],
   rules: {
+    "node/no-missing-import": 0,
     semi: ['error', 'never'],
     "react/jsx-filename-extension": 0
   }
