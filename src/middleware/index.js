efc625afe57729363fcec4e4e080105424fc7680
viewar_sf <dev@project23.org>
2019-11-30 00:48:20 +0100

    refactor(viewarCoreMiddleware): move mock-restriction from index to `viewarCoreMiddleware`

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -4,10 +4,7 @@ const { viewarCoreMiddleware } = require('./viewarCore')
 
 const viewArMiddleware = (app) => {
   remoteConsoleMiddleware(app)
-
-  if (process.env.WEBPACK_ENV !== 'development_mock') {
-    viewarCoreMiddleware(app)
-  }
+  viewarCoreMiddleware(app)
 }
 
 
