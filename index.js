b6c2e037cced33ce91632bb982578b954f9844b7
viewar_sf <dev@project23.org>
2019-11-30 01:57:14 +0100

    add named exports for helpers

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const viewarWebpackConfigAsync = require('./src/webpack.config.js')
+const viewarWebpackConfig = require('./src/webpack.config.js')
+const remoteConsoleInjector = require('./src/utils/remoteConsole/remoteConsoleInjector')
+const { remoteConsoleMiddleware } = require('./src/utils/remoteConsole/remoteConsoleMiddleware')
+const { viewArMiddleware } = require('./src/middleware/viewarCore')
 
-module.exports = viewarWebpackConfigAsync
+// * default export
+const ViewarWebpack = module.exports = viewarWebpackConfig
+
+// * named exports
+ViewarWebpack.viewArMiddleware = viewArMiddleware
+ViewarWebpack.remoteConsoleInjector = remoteConsoleInjector
+ViewarWebpack.remoteConsoleMiddleware = remoteConsoleMiddleware
