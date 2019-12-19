ee46e65b3d552d2e50547b6d3ea312af87230664
SF <sf@viewar.com>
2019-12-19 14:20:50 +0100

    refactor(assets): move assets to workspace root

--- src/env/common.config.js
+++ src/env/common.config.js
@@ -81,7 +81,7 @@ const getCommonConfig = (env) => {
             use:  {
               loader:  'file-loader',
               options: {
-                name:       '[path]/[name].[ext]',
+                name:       '[path][name].[ext]',
                 publicPath: '/', // server path in DEV
                 outputPath: '/', // fs path in PROD
               },
