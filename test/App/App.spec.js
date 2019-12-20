32a0cf66d3337eacabc9ce4ac289af6ad2a7d52f
viewar_sf <dev@project23.org>
2019-12-20 21:52:45 +0100

    ci(tests): move asset tests to '__tests__'

--- test/App/App.spec.js
+++ test/App/App.spec.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 import App from './App'
 import Test from '../components/Test'
-import * as assets from '../../assets'
 
 const { enzyme: { mount }, chai: { expect }} = global
 
@@ -13,22 +12,10 @@ describe('<App />', function() {
     expect(wrapper).to.have.descendants('#app_headline')
   })
 
-  it('renders with child component <Test />', function() {
+  it('renders with child component <Test foo="foo" />', function() {
     expect(wrapper).to.contain(<Test foo="bar" />)
     expect(wrapper).to.not.contain(<Test foo="foo" />)
 
     expect(wrapper).to.have.descendants('#foo')
-  })
-})
-
-// TODO: move asset test to __tests__
-describe('Assets - file-loader', function() {
-  it('should export-default-from and import as namespace', () => {
-    // check export-default-from
-    expect(assets.__esModule).to.be.equal(true) // eslint-disable-line import/namespace
-    // check import - import-namespace combined with export-default-from
-    expect(assets).to.contain.keys('viewar_device_assembly')
-    // check path prefix
-    expect(assets.viewar_device_assembly).to.contain('/assets/viewar_device_assembly.png')
   })
 })
