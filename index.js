39b8498430670cf9c00132bf797528fc87c7d8f2
Karl Hofer <kh@viewar.com>
2020-02-11 14:12:24 +0100

    fix(viewarCore): correctly load viewar-core.js after npm run start.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-const viewarWebpackConfig = require('./src/webpack.config.js')
+const viewarWebpackConfig = require('./src/webpack.config.js');
+const packageJson = require('./package.json');
 
-module.exports = viewarWebpackConfig
+console.info(`ViewAR Version: @viewar/webpack v${packageJson.version}`);
+
+module.exports = viewarWebpackConfig;
