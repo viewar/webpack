e61933695042e42785f1b372bb4d29fa9cce632b
viewar_sf <dev@project23.org>
2020-02-24 16:20:18 +0100

    fix(lint-staged): use eslint for js files

--- src/env/develop.config.js
+++ src/env/develop.config.js
@@ -1,37 +1,37 @@
-const merge = require('webpack-merge');
-const ip = require('ip').address();
-const webpack = require('webpack');
+const merge = require('webpack-merge')
+const ip = require('ip').address()
+const webpack = require('webpack')
 
-const viewArMiddleware = require('../middlewares');
-const { setEnvVariable } = require('../utils');
-const { PATHS, PORT } = require('../constants');
-const { printLaunchQRCode } = require('../utils');
+const viewArMiddleware = require('../middlewares')
+const { setEnvVariable } = require('../utils')
+const { PATHS, PORT } = require('../constants')
+const { printLaunchQRCode } = require('../utils')
 
 const getDevelopConfig = () => {
-  printLaunchQRCode(ip, PORT);
-  setEnvVariable('process.env.NODE_ENV', 'development');
+  printLaunchQRCode(ip, PORT)
+  setEnvVariable('process.env.NODE_ENV', 'development')
 
   return merge([
     {
       devServer: {
-        public: ip ? `${ip}:${PORT}` : null,
-        host: process.env.HOST || '0.0.0.0',
-        port: PORT,
+        'public':      ip ? `${ip}:${PORT}` : null,
+        host:        process.env.HOST || '0.0.0.0',
+        port:        PORT,
         contentBase: PATHS.build,
-        hot: true,
-        overlay: {
+        hot:         true,
+        overlay:     {
           warnings: false,
-          errors: true,
+          errors:   true,
         },
         before: viewArMiddleware,
       },
       devtool: 'inline-source-map',
-      output: {
+      output:  {
         devtoolModuleFilenameTemplate: 'webpack:///[absolute-resource-path]',
       },
-      plugins: [new webpack.HotModuleReplacementPlugin()],
+      plugins: [ new webpack.HotModuleReplacementPlugin() ],
     },
-  ]);
-};
+  ])
+}
 
-module.exports = getDevelopConfig;
+module.exports = getDevelopConfig
