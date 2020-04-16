3a92237510469032963dd93180d9793954ee30e8
viewar_sf <dev@project23.org>
2020-04-16 11:24:33 +0200

    adapt resolve config to 'TsconfigPathsPlugin'

--- src/webpack.config.resolve.js
+++ src/webpack.config.resolve.js
@@ -1,12 +1,35 @@
-const { basename, join } = require('path')
+// const { basename, join } = require('path')
+
+// const { PATHS } = require('./constants')
+// // TODO: check for 'resolver.config.js' in root/configs
+
+// const resolveConfig = {
+//   resolve: {
+//     extensions: [ '.ts', '.tsx', '.js', '.jsx' ],
+//     modules:    [ join(basename(PATHS.src), 'components'), basename(PATHS.src), 'node_modules' ],
+//   },
+// }
+
+// module.exports = resolveConfig
+
+const { join } = require('path')
+
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
 const { PATHS } = require('./constants')
-// TODO: check for 'resolver.config.js' in root/configs
 
 const resolveConfig = {
   resolve: {
-    extensions: [ '.js', '.jsx', 'json' ],
-    modules:    [ join(basename(PATHS.src), 'components'), basename(PATHS.src), 'node_modules' ],
+    extensions: [ '.ts', '.tsx', '.js', '.jsx' ],
+    // ? already included into tsconfigs paths
+    // alias:      {
+    //   assets: PATHS.assets,
+    // },
+    plugins:    [
+      new TsconfigPathsPlugin({
+        configFile: join(PATHS.root, 'tsconfig.json'),
+      }),
+    ],
   },
 }
 
