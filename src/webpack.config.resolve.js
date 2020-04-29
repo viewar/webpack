371cd60cbf6ca93512989b88948b7abc817f6cd3
viewar_sf <dev@project23.org>
2020-04-29 14:51:01 +0200

    resolve-config: use default if tsconfig is !found

--- src/webpack.config.resolve.js
+++ src/webpack.config.resolve.js
@@ -1,4 +1,4 @@
-const { join } = require('path')
+const { basename, join } = require('path')
 
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
@@ -6,29 +6,38 @@ const { PATHS } = require('./constants')
 
 const resolveConfig = {
   resolve: {
-    extensions: [ '.ts', '.tsx', '.js', '.jsx' ],
-    alias:      { // ? already included into tsconfigs paths
-      assets: PATHS.assets,
-    },
+    extensions: [ '.js', '.jsx', '.ts', '.tsx', '.json' ],
   },
 }
 
 // check if tsconfig is present
 const tsconfigPath = join(PATHS.root, 'tsconfig.json')
+let tsconfigIsPresent = false
 try {
   require(tsconfigPath)
+  tsconfigIsPresent = true
   if (process.env.DEBUG) {
     console.log('\n[@viewar/webpack] tsconfig is PRESENT, so we USE \'tsconfig-paths-webpack-plugin\'\n\n')
   }
-
-  resolveConfig.resolve.plugins = [ new TsconfigPathsPlugin({
-    configFile: tsconfigPath,
-  }) ]
 }
 catch (e) {
   if (process.env.DEBUG) {
     console.log('\n[@viewar/webpack] tsconfig is NOT PRESENT, so we DON\'T USE \'tsconfig-paths-webpack-plugin\'\n\n')
   }
+}
+
+if (tsconfigIsPresent) {
+  // use tsconfig-paths-webpack-plugin
+  resolveConfig.resolve.plugins = [ new TsconfigPathsPlugin({
+    configFile: tsconfigPath,
+  }) ]
+}
+else {
+  // use default module resolve config
+  resolveConfig.resolve.alias = { assets: PATHS.assets }
+  resolveConfig.resolve.modules = [
+    basename(PATHS.src), 'node_modules',
+  ]
 }
 
 module.exports = resolveConfig
